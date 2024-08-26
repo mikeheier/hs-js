8477bec349b10ecb329023fc975938250894c95f
Michael Heier <michael.heier@mind-over-data.com>
2024-08-26 09:14:03 -0500

    version 0.0.6

---
+++ src/model/event/HsCustomEvent.js
@@ -0,0 +1,40 @@
+class HsCustomEvent extends CustomEvent {
+   constructor(type, originalEvent, data) {
+      super(type || originalEvent?.type || 'hsevent', {
+         detail: data,
+         cancelable: true
+      });
+
+      this.originalEvent = originalEvent;
+
+      Object
+         .keys(data ?? {})
+         .forEach((key) => {
+            this[key] = data[key];
+         });
+   }
+
+   preventDefault() {
+      super.preventDefault();
+
+      if (this.originalEvent?.preventDefault) {
+         this.originalEvent.preventDefault();
+      }
+   }
+
+   stopImmediatePropagation() {
+      super.stopImmediatePropagation();
+
+      if (this.originalEvent?.stopImmediatePropagation) {
+         this.originalEvent.stopImmediatePropagation();
+      }
+   }
+
+   stopPropagation() {
+      super.stopPropagation();
+
+      if (this.originalEvent?.stopPropagation) {
+         this.originalEvent.stopPropagation();
+      }
+   }
+}
